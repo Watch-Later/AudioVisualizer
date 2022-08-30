b5f7fc50d5098c7df2f0e8a272f7658f2678117d
haku-c <kokoanimal@gmail.com>
2022-08-30 18:53:02 -0400

    Add project files.

---
+++ PlayFiles/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace PlayFiles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.urlText = new System.Windows.Forms.TextBox();
+            this.startButton = new System.Windows.Forms.Button();
+            this.pauseButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // urlText
+            // 
+            this.urlText.Location = new System.Drawing.Point(342, 96);
+            this.urlText.Name = "urlText";
+            this.urlText.Size = new System.Drawing.Size(100, 22);
+            this.urlText.TabIndex = 1;
+            this.urlText.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(356, 145);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 2;
+            this.startButton.Text = "Play";
+            this.startButton.UseVisualStyleBackColor = true;
+            // 
+            // pauseButton
+            // 
+            this.pauseButton.Location = new System.Drawing.Point(356, 185);
+            this.pauseButton.Name = "pauseButton";
+            this.pauseButton.Size = new System.Drawing.Size(75, 23);
+            this.pauseButton.TabIndex = 3;
+            this.pauseButton.Text = "Pause";
+            this.pauseButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pauseButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.urlText);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox urlText;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button pauseButton;
+    }
+}
+
---
+++ PlayFiles/Form1.cs
@@ -0,0 +1,83 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PlayFiles
+{
+    public partial class Form1 : Form
+    {
+        string song;
+        WaveOut output;
+        Mp3FileReader reader;
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            urlText.Text = "Enter url";
+            urlText.GotFocus += RemoveText;
+            urlText.LostFocus += AddText;
+            startButton.Click += PlaySong;
+            pauseButton.Click += PauseSong;
+
+        }
+
+        public void RemoveText(object sender, EventArgs e)
+        {
+            if (urlText.Text == "Enter url")
+            {
+                urlText.Text = "";
+            }
+        }
+
+        public void AddText(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(urlText.Text))
+                urlText.Text = "Enter text here...";
+        }
+
+        public void PlaySong(object sender, EventArgs e)
+        {
+            if (song == urlText.Text)
+            {
+                return;
+            }
+            song = urlText.Text;
+            output = new WaveOut();
+            reader = new Mp3FileReader(song);
+            output.Init(reader);
+            output.Play();
+            pauseButton.Text = "Pause";
+        }
+        public void PauseSong(object sender, EventArgs e)
+        {
+            if (output == null)
+            {
+                return;
+            }
+            if (output.PlaybackState == PlaybackState.Playing)
+            {
+                output.Pause();
+                pauseButton.Text = "Resume";
+            }
+            else
+            {
+                output.Play();
+                pauseButton.Text = "Start";
+            }
+        }
+    }
+
+}
